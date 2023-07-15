e541a0a106544f23346858ff045aefe8ebb80833
Ashwini Masilamani <ashwinimasil@gmail.com>
2023-07-15 21:09:14 +0100

    Print statement added in Delete individual

--- src/main/java/pages/TC006_DeleteIndividuals.java
+++ src/main/java/pages/TC006_DeleteIndividuals.java
@@ -9,6 +9,7 @@ public class TC006_DeleteIndividuals extends BaseClass {
 	@And("Click on the Delete option in the displayed popup window")
 	public TC003_IndividualsPage popUpDelete() {
 		driver.findElement(By.xpath("//span[text()='Delete']")).click();
+		System.out.println("Delete Individuals");
 		return new TC003_IndividualsPage();
 	}
 }
